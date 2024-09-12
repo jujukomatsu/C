81f0566f976386561d0f207f6794dc616e8aa02d
jujukomatsu <jujurjkomatsu@gmail.com>
2024-09-12 17:04:06 -0300

    up

---
+++ faculdade/exercicios/5_triangulo/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int lado1, lado2, lado3;
+
+    printf("Digite o primeiro lado:");
+    scanf("%d", &lado1);
+    printf("Digite o segundo lado:");
+    scanf("%d", &lado2);
+    printf("Digite o terceiro lado:");
+    scanf("%d", &lado3);
+    if (lado1 == lado2 && lado2 == lado3 && lado1 == lado3)
+    {
+        printf("equilátero");
+    }
+    else
+    {
+        if (lado1 == lado2 || lado3 == lado2 || lado3 == lado1)
+        {
+            printf("isósceles");
+        }
+        else
+        {
+            if (lado1 != lado2 && lado3 != lado1 || lado3 != lado2)
+            {
+                printf("escaleno");
+            }
+        }
+    }
+
+    return 0;
+}
